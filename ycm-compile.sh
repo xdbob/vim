dc0949249f23f2e52a2f4ebe27b4c16a74ae0f49
Antoine Damhet <antoine.damhet@lse.epita.fr>
2016-06-13 15:53:21 +0200

    YCM check dependancies.
    Signed-off-by: Antoine Damhet <antoine.damhet@lse.epita.fr>

--- ycm-compile.sh
+++ ycm-compile.sh
@@ -1,13 +1,34 @@
 #!/bin/sh
 
 if [ ! -e bundle/YouCompleteMe/install.py ]; then
-  echo YouCompleteMe not found...
-  echo Have you run \"git submodule update --init --recursive\" ?
-  exit 1;
+	echo YouCompleteMe not found...
+	echo Have you run \"git submodule update --init --recursive\" ?
+	exit 1;
 fi
 echo YouCompleteMe found.
 
 cd bundle/YouCompleteMe
-python2 install.py --clang-completer --omnisharp-completer --gocode-completer
+
+ARGS="--clang-completer"
+
+echo -n "Is mono present ? "
+which mono > /dev/null
+if [ $? -eq 0 ]; then
+	echo "yes"
+	ARGS="${ARGS} --omnisharp-completer"
+else
+	echo "no"
+fi
+
+echo -n "Is go present ? "
+which go > /dev/null
+if [ $? -eq 0 ]; then
+	echo "yes"
+	ARGS="${ARGS} --gocode-completer"
+else
+	echo "no"
+fi
+
+python2 install.py $ARGS
 
 exit $?
